0d930cf5af2bef08c0b75aae9f491b1732351807
youcova <helloworld.cova@gmail.com>
2022-07-31 18:48:27 +0900

    柴犬の画像を表示可能に実装。shiba API GETメソッドから画像表示させる処理。

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,21 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MaterialApp(home: HomePage()));
+}
+
+Future<ShibaPic> fetchShibaPic() async {
+  final response = await http.get(Uri.parse(
+      'http://shibe.online/api/shibes?count=[1]&urls=[false]&httpsUrls=[true]'));
+  if (response.statusCode == 200) {
+    print(response.body);
+    return ShibaPic.fromJson(jsonDecode(response.body));
+  } else {
+    throw Exception('柴犬画像取得できず');
+  }
 }
 
 class HomePage extends StatefulWidget {
@@ -12,23 +26,60 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  late Future<ShibaPic> futurePic;
+
+  @override
+  void initState() {
+    super.initState();
+    futurePic = fetchShibaPic();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: const Text("shiba"),
         ),
-        body: Column(
+        body: Center(
+            child: Column(
           children: <Widget>[
-            const Image(
-                image: NetworkImage(
-                    'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg')
-            ),
+            FutureBuilder<ShibaPic>(
+                future: futurePic,
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.done) {
+                    if (snapshot.hasError) {
+                      return Text("Error: ${snapshot.error}");
+                    }
+                    if (!snapshot.hasData) {
+                      return Text("データが見つかりません");
+                    }
+                    // データ表示
+                    return Image(
+                        image: NetworkImage(
+                            'https://cdn.shibe.online/shibes/${snapshot.data!.picId}.jpg'));
+                  } else {
+                    // 処理中の表示
+                    return const CircularProgressIndicator();
+                  }
+                }),
             ElevatedButton(
-                child: const Text('柴犬'),
-                onPressed: () {},
+              child: const Text('柴犬'),
+              onPressed: () {},
             ),
           ],
-        ));
+        )));
+  }
+}
+
+///
+/// 柴犬の画像クラス
+///
+class ShibaPic {
+  final String picId;
+
+  const ShibaPic({required this.picId});
+
+  factory ShibaPic.fromJson(List<dynamic> json) {
+    return ShibaPic(picId: json[0] as String);
   }
 }
